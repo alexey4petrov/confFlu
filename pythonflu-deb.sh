1827f148b730fbd6ee3b4cea2dffbc2567ee26bd
asimurzin <asimurzin@gmail.com>
2011-02-10 11:30:39 +0000

    to separate on different steps

--- pythonflu-deb.sh
+++ pythonflu-deb.sh
@@ -23,23 +23,12 @@
 
 
 #--------------------------------------------------------------------------------------
-usage="\
-
-Usage: $0 --foam-version=<foam version> --build-version=<build version> [--upload] [--step=<step name>]
-Parameters:
-       --foam_version=<foam version> - version of the OpenFOAM( 1.5-dev, 1.6-ext,1.7.0, 1.7.1 etc )
-       --build-version=<build version> - our build number
-       --upload    create source package and upload it to the launchpad( or if It not exists create deb package )
-       --step=<step name> - we begin with this <step name> ( checkout, build_configure, configure, make, deb )
-Options:
-       --help     display this help and exit.
-
-"
-
-
-#--------------------------------------------------------------------------------------
 install_foam()
 {
+   foam_package_name=$1
+   supported_codenames=$2
+   foam_url=$3
+   
    os_codename=`lsb_release -a 2>/dev/null | grep Codename | sed 's/Codename:\t//'`
    os_support=false
    for codename in ${supported_codenames}; do
@@ -66,24 +55,125 @@ install_foam()
 #--------------------------------------------------------------------------------------
 source_foam()
 {
-  foam_version=${1}
-  foam_package_name=${2}
-  supported_codenames=${3} 
-  foam_url=${4}
+  foam_package_name=${1}
+  supported_codenames=${2} 
+  foam_url=${3}
   foam_exist=`dpkg -s ${foam_package_name} 2>/dev/null`
 
   if [ "x${foam_exist}" == "x" ]; then
-     install_foam ${foam_version} ${foam_package_name} "${supported_codenames}" ${foam_url} 
+     install_foam ${foam_package_name} "${supported_codenames}" ${foam_url} 
   fi
 
   path_to_foam_bashrc=`dpkg -L ${foam_package_name} | grep etc/bashrc`
   source ${path_to_foam_bashrc}
-  
 }
 
 
 #--------------------------------------------------------------------------------------
-#Check is the first argument "--help" and then checking enviroment( CONFFOAM_ROOT_DIR exists )
+check_openfoam()
+{
+  foam_version=$1
+  if [ "x${WM_PROJECT_VERSION}" != "x${foam_version}" ]; then
+     echo "$0: The current OpenFOAM version is \"${WM_PROJECT_VERSION}\" and not equal \"${foam_version}\". It is necessary to source OpenFOAM-${foam_version}\
+          or define another OpenFAOM in command line with --foam-version=" >&2
+     exit -1
+  fi
+  
+  export PATH_TO_FOAM_BASHRC=${WM_PROJECT_DIR}/etc/bashrc
+}
+
+
+#--------------------------------------------------------------------------------------
+checkout_pythonflu()
+{
+  echo "-------------------------------------------------------------"
+  echo " checkout pythonFlu....."
+  echo "-------------------------------------------------------------"
+  
+  check_openfoam ${foam_version}
+  working_folder=`pwd`/pythonflu-deb-${foam_version}-${build_version}
+
+  if [ -d ${working_folder} ]; then
+     rm -rf ${working_folder}
+  fi
+  mkdir ${working_folder}
+  
+  pythonflu_folder=${working_folder}/pythonflu
+  mkdir ${pythonflu_folder}
+  
+  svn co https://afgan.svn.sourceforge.net/svnroot/afgan/pyfoam/branches/r928_deb_packages/ ${pythonflu_folder}
+  
+  echo "source ${CONFFOAM_ROOT_DIR}/bashrc" > ${pythonflu_folder}/env_new.sh
+  echo "source_openfoam ${PATH_TO_FOAM_BASHRC}" >> ${pythonflu_folder}/env_new.sh
+  
+  mv ${pythonflu_folder}/env.sh ${pythonflu_folder}/env_old.sh
+  mv ${pythonflu_folder}/env_new.sh ${pythonflu_folder}/env.sh
+  echo "-------------------------------------------------------------"
+}
+
+
+
+#--------------------------------------------------------------------------------------
+build_configure_pythonflu()
+{
+  working_folder=`pwd`/pythonflu-deb-${foam_version}-${build_version}
+  pythonflu_folder=${working_folder}/pythonflu
+  
+  bashrc_string=`cat ${pythonflu_folder}/env.sh | grep $CONFFOAM_ROOT_DIR`
+  if  [ "x${bashrc_string}" = "x" ]; then
+     echo "$0: There is no \"correct\" modified ${pythonflu_folder}/env.sh. It is necessary to checkout pythonFlu ( --step=checkout ) "
+     exit -1;
+  fi
+  source ${pythonflu_folder}/env.sh
+  ( cd ${pythonflu_folder} && build_configure )
+}
+
+
+#--------------------------------------------------------------------------------------
+configure_pythonflu()
+{
+  working_folder=`pwd`/pythonflu-deb-${foam_version}-${build_version}
+  pythonflu_folder=${working_folder}/pythonflu
+  if [ -f ${pythonflu_folder}/configure ]; then
+       source ${pythonflu_folder}/env.sh
+      ( cd ${pythonflu_folder} && ./configure build_version=${build_version} --disable-singlelib )
+  else
+     echo "$0: There is no \"configure\" file in \" ${pythonflu_folder}\". It is necessary to run build_configure pythonFlu ( --step=build_configure )"
+     exit -1
+  fi
+}
+
+#--------------------------------------------------------------------------------------
+make_pythonflu()
+{
+  working_folder=`pwd`/pythonflu-deb-${foam_version}-${build_version}
+  pythonflu_folder=${working_folder}/pythonflu
+  if [ -f ${pythonflu_folder}/bashrc ]; then
+     source ${pythonflu_folder}/bashrc
+     ( cd ${pythonflu_folder} && make )
+  else
+     echo "$0: There is no \"bashrc\" file in \" ${pythonflu_folder}\". It is necessary to run configure pythonFlu ( --step=configure )"
+     exit -1
+  fi
+  
+}
+
+#--------------------------------------------------------------------------------------
+usage="\
+
+Usage: $0 --foam-version=<foam version> --build-version=<build version> [--upload] [--step=<step name>]
+Parameters:
+       --foam_version=<foam version> - version of the OpenFOAM( 1.5-dev, 1.6-ext,1.7.0, 1.7.1 etc )
+       --build-version=<build version> - our build number
+       --upload    create source package and upload it to the launchpad( or if It not exists create deb package )
+       --step=<step name> - we begin with this <step name> ( checkout, build_configure, configure, make, deb )
+Options:
+       --help     display this help and exit.
+
+"
+
+#--------------------------------------------------------------------------------------
+#Check is the first argument "--help" and then checking enviroment( variable CONFFOAM_ROOT_DIR exists )
 if [ "x$1" = "x--help" ]; then
    echo "${usage}"
    exit 0
@@ -172,21 +262,34 @@ case ${foam_version} in
    ;;
 esac
 
+export foam_version=${foam_version}
+export build_version=${build_version}
+
 case ${step} in
-   All)
-      echo ${step};
+   all)
+      source_foam ${foam_package_name} "${supported_codenames}" ${foam_url}
+      checkout_pythonflu
+      build_configure_pythonflu
+      configure_pythonflu
+      make_pythonflu 
    ;;   
    checkout)
-      echo ${step};
+      checkout_pythonflu
+      build_configure_pythonflu
+      configure_pythonflu
+      make_pythonflu
    ;;
    build_configure)
-      echo ${step};
+      build_configure_pythonflu
+      configure_pythonflu 
+      make_pythonflu
    ;;
    configure)
-      echo ${step}; 
+      configure_pythonflu 
+      make_pythonflu
    ;;
    make)
-      echo ${step}; 
+      make_pythonflu
    ;;
    deb)
       echo ${step}; 
@@ -197,25 +300,4 @@ case ${step} in
    ;;
 esac
 
-
-#------------------------------------------------------------------------------------------------
-# check openfoam and install it, if not installed, then source it 
-
-source_foam ${foam_version} ${foam_package_name} "${supported_codenames}" ${foam_url} 
-echo $WM_PROJECT_VERSION
-
-#------------------------------------------------------------------------------------------------
-#checkout and build pythonflu
-work_folder=`pwd`/pythonflu-deb-${foam_version}-${build_version}
-mkdir ${work_folder}
-
-conffoam_folder=${work_folder}/conffoam
-mkdir ${conffoam_folder}
-
-pythonflu_folder=${work_folder}/pythonflu
-mkdir ${pythonflu_folder}
-
-#svn co https://afgan.svn.sourceforge.net/svnroot/afgan/pyfoam/trunk/ ${pythonflu_folder}
-svn co https://afgan.svn.sourceforge.net/svnroot/afgan/pyfoam/branches/r928_deb_packages/ ${pythonflu_folder}
-(cd ${pythonflu_folder} && build_configure && ./configure )
 
