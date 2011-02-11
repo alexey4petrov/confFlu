c5a25a1e577058e8e71d34a3e876762fecbbce2c
asimurzin <asimurzin@gmail.com>
2011-02-11 07:08:32 +0000

    1) remove unnecessary folder "pythonflu" from "pythonflu-dev-OFversion-build-version" 2) to add option pgp_key_id 3) to add step make_deb

--- pythonflu-deb.sh
+++ pythonflu-deb.sh
@@ -90,23 +90,19 @@ checkout_pythonflu()
   echo "-------------------------------------------------------------"
   
   check_openfoam 
-  working_folder=`pwd`/pythonflu-deb-${__FOAM_VERSION__}-${__BUILD_VERSION__}
 
-  if [ -d ${working_folder} ]; then
-     rm -rf ${working_folder}
+  if [ -d ${__PYTHONFLU_FOLDER__} ]; then
+     rm -rf ${__PYTHONFLU_FOLDER__}
   fi
-  mkdir ${working_folder}
+  mkdir ${__PYTHONFLU_FOLDER__}
   
-  pythonflu_folder=${working_folder}/pythonflu
-  mkdir ${pythonflu_folder}
+  svn co https://afgan.svn.sourceforge.net/svnroot/afgan/pyfoam/branches/r928_deb_packages/ ${__PYTHONFLU_FOLDER__}
   
-  svn co https://afgan.svn.sourceforge.net/svnroot/afgan/pyfoam/branches/r928_deb_packages/ ${pythonflu_folder}
+  echo "source ${CONFFOAM_ROOT_DIR}/bashrc" > ${__PYTHONFLU_FOLDER__}/env_new.sh
+  echo "source_openfoam ${PATH_TO_FOAM_BASHRC}" >> ${__PYTHONFLU_FOLDER__}/env_new.sh
   
-  echo "source ${CONFFOAM_ROOT_DIR}/bashrc" > ${pythonflu_folder}/env_new.sh
-  echo "source_openfoam ${PATH_TO_FOAM_BASHRC}" >> ${pythonflu_folder}/env_new.sh
-  
-  mv ${pythonflu_folder}/env.sh ${pythonflu_folder}/env_old.sh
-  mv ${pythonflu_folder}/env_new.sh ${pythonflu_folder}/env.sh
+  mv ${__PYTHONFLU_FOLDER__}/env.sh ${__PYTHONFLU_FOLDER__}/env_old.sh
+  mv ${__PYTHONFLU_FOLDER__}/env_new.sh ${__PYTHONFLU_FOLDER__}/env.sh
   echo "-------------------------------------------------------------"
 }
 
@@ -115,29 +111,24 @@ checkout_pythonflu()
 #--------------------------------------------------------------------------------------
 build_configure_pythonflu()
 {
-  working_folder=`pwd`/pythonflu-deb-${__FOAM_VERSION__}-${__BUILD_VERSION__}
-  pythonflu_folder=${working_folder}/pythonflu
-  
-  bashrc_string=`cat ${pythonflu_folder}/env.sh | grep $CONFFOAM_ROOT_DIR`
+  bashrc_string=`cat ${__PYTHONFLU_FOLDER__}/env.sh | grep $CONFFOAM_ROOT_DIR`
   if  [ "x${bashrc_string}" = "x" ]; then
-     echo "$0: There is no \"correct\" modified ${pythonflu_folder}/env.sh. It is necessary to checkout pythonFlu ( --step=checkout ) "
+     echo "$0: There is no \"correct\" modified ${__PYTHONFLU_FOLDER__}/env.sh. It is necessary to checkout pythonFlu ( --step=checkout ) " >&2
      exit -1;
   fi
-  source ${pythonflu_folder}/env.sh
-  ( cd ${pythonflu_folder} && build_configure )
+  source ${__PYTHONFLU_FOLDER__}/env.sh
+  ( cd ${__PYTHONFLU_FOLDER__} && build_configure )
 }
 
 
 #--------------------------------------------------------------------------------------
 configure_pythonflu()
 {
-  working_folder=`pwd`/pythonflu-deb-${__FOAM_VERSION__}-${__BUILD_VERSION__}
-  pythonflu_folder=${working_folder}/pythonflu
-  if [ -f ${pythonflu_folder}/configure ]; then
-       source ${pythonflu_folder}/env.sh
-      ( cd ${pythonflu_folder} && ./configure build_version=${__BUILD_VERSION__} --disable-singlelib )
+  if [ -f ${__PYTHONFLU_FOLDER__}/configure ]; then
+       source ${__PYTHONFLU_FOLDER__}/env.sh
+      ( cd ${__PYTHONFLU_FOLDER__} && ./configure build_version=${__BUILD_VERSION__} pgp_key_id=${__PGP_KEY_ID__} --disable-singlelib )
   else
-     echo "$0: There is no \"configure\" file in \" ${pythonflu_folder}\". It is necessary to run build_configure pythonFlu ( --step=build_configure )"
+     echo "$0: There is no \"configure\" file in \" ${__PYTHONFLU_FOLDER__}\". It is necessary to run build_configure pythonFlu ( --step=build_configure )" >&2
      exit -1
   fi
 }
@@ -146,26 +137,37 @@ configure_pythonflu()
 #--------------------------------------------------------------------------------------
 make_pythonflu()
 {
-  working_folder=`pwd`/pythonflu-deb-${__FOAM_VERSION__}-${__BUILD_VERSION__}
-  pythonflu_folder=${working_folder}/pythonflu
-  if [ -f ${pythonflu_folder}/bashrc ]; then
-     source ${pythonflu_folder}/bashrc
-     ( cd ${pythonflu_folder} && make )
+  if [ -f ${__PYTHONFLU_FOLDER__}/bashrc ]; then
+     source ${__PYTHONFLU_FOLDER__}/bashrc
+     ( cd ${__PYTHONFLU_FOLDER__} && make )
   else
-     echo "$0: There is no \"bashrc\" file in \" ${pythonflu_folder}\". It is necessary to run configure pythonFlu ( --step=configure )"
+     echo "$0: There is no \"bashrc\" file in \" ${__PYTHONFLU_FOLDER__}\". It is necessary to run configure pythonFlu ( --step=configure )" >&2
      exit -1
   fi
   
 }
 
+
+#--------------------------------------------------------------------------------------
+make_deb()
+{
+   if [ "${__UPLOAD__}" = "true" ]; then
+      ( cd ${__PYTHONFLU_FOLDER__} && make launchpad )
+   else
+      ( cd ${__PYTHONFLU_FOLDER__} && make deb )
+   fi
+}
+
+
 #--------------------------------------------------------------------------------------
 usage="\
 
-Usage: $0 --foam-version=<foam version> --build-version=<build version> [--upload] [--step=<step name>]
+Usage: $0 --foam-version=<foam version> --build-version=<build version> [--upload --pgp_key_id=<pgp key id>] [--step=<step name>]
 Parameters:
        --foam_version=<foam version> - version of the OpenFOAM( 1.5-dev, 1.6-ext,1.7.0, 1.7.1 etc )
        --build-version=<build version> - our build number
-       --upload    create source package and upload it to the launchpad( or if It not exists create deb package )
+       --upload  - create source package and upload it to the launchpad( or if It not exists create deb package )
+       --pgp_key_id - pgp key id to sign package, to find it to run \"gpg --list-keys\"
        --step=<step name> - we begin with this <step name> ( checkout, build_configure, configure, make, deb )
 Options:
        --help     display this help and exit.
@@ -209,6 +211,12 @@ for arg in $* ;  do
       upload=true
       correct_arg=true
    fi  
+   
+   if [ "`echo $arg | grep --regexp='--pgp_key_id='`" ]; then
+      pgp_key_id=`echo ${arg} | awk "-F=" '{print $2}'`
+      correct_arg=true
+   fi  
+
    if [ "`echo $arg | grep --regexp='--step='`" ]; then
       step=`echo ${arg} | awk "-F=" '{print $2}'`
       correct_arg=true
@@ -230,6 +238,19 @@ if [ "${build_version_exist}" == "false" ]; then
    echo "${usage}"; exit -1
 fi
 
+if [ "${upload}" = "true" ]; then
+   if [ "x${pgp_key_id}" = "x" ]; then
+      echo "$0: empty pgp key id. If you choice \"--upload\" option, it is necessary to define correct pgp key id. " >&2
+      exit -1;
+   fi
+   
+   count_pgp_key_id=0
+   count_pgp_key_id=`gpg --list-keys | grep -c "${pgp_key_id}"`
+   if [ ${count_pgp_key_id} -eq 0 ]; then
+      echo "$0: There is no PGP key with id \"${pgp_key_id}\" in your system. If you choice \"--upload\" option, it is necessary to define correct pgp key id " >&2
+      exit -1;
+   fi
+fi
 
 foam_package_name=""
 supported_codenames=""
@@ -264,6 +285,9 @@ esac
 
 export __FOAM_VERSION__=${foam_version}
 export __BUILD_VERSION__=${build_version}
+export __PGP_KEY_ID__=${pgp_key_id}
+export __PYTHONFLU_FOLDER__=`pwd`/pythonflu-deb-${__FOAM_VERSION__}-${__BUILD_VERSION__}
+export __UPLOAD__=${upload}
 
 case ${step} in
    all)
@@ -271,28 +295,33 @@ case ${step} in
       checkout_pythonflu
       build_configure_pythonflu
       configure_pythonflu
-      make_pythonflu 
+      make_pythonflu
+      make_deb
    ;;   
    checkout)
       checkout_pythonflu
       build_configure_pythonflu
       configure_pythonflu
       make_pythonflu
+      make_deb
    ;;
    build_configure)
       build_configure_pythonflu
       configure_pythonflu 
       make_pythonflu
+      make_deb
    ;;
    configure)
       configure_pythonflu 
       make_pythonflu
+      make_deb
    ;;
    make)
       make_pythonflu
+      make_deb
    ;;
    deb)
-      echo ${step}; 
+      make_deb
    ;;
    *)
       echo "$0: Unknown option step=${step}. The step option can take values \"checkout\", \"build_configure\", \"configure\",\"make\",\"deb\"." >&2
